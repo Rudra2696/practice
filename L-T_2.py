2f8e86d3708b387910c9981ed730a61870d19923
Rudra Patel <rhpatel1023@gmail.com>
2025-03-16 11:46:47 +0530

    displaing mount everest

--- L-T_2.py
+++ L-T_2.py
@@ -1,4 +1,5 @@
 import folium
 
-map=folium.Map(location=[-50,150],zoom_start=4)
+map=folium.Map(location=[28.3949,84.1240],zoom_start=8)
+map.add_child(folium.Marker(location=[27.9881,86.9250],popup="Mount Everest",icon=folium.Icon(color="green")))
 map.save("map.html")
