ba2919a04e5ffc0e4556bb048a283c27c1c7752b
Rudra Patel <rhpatel1023@gmail.com>
2025-03-18 18:12:07 +0530

    bug fixed

--- task_tracker.py
+++ task_tracker.py
@@ -10,10 +10,17 @@ while True:
 
 l=[]
 for i in range(1,n+1):
-    z=input(f"Enter task {i} : ")
-    m=z.capitalize().strip()
+    while True:
+        z=input(f"Enter your task {i} : ")    
+        m=z.capitalize().strip()
+        if m=="" :
+            print("Please enter your task ")
+        else:
+            break
     l.append(m)
     
 
-print(f"Your task list is {l}")       
+for i,j in zip(range(1,n+1),l):
+    print(f"Your task {i} is {j}.")
+        
 
