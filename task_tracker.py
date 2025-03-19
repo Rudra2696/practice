f24875b3f1df6fb2ac66abf2e564e526ca2c14b7
Rudra Patel <rhpatel1023@gmail.com>
2025-03-19 16:05:32 +0530

    added new features

--- task_tracker.py
+++ task_tracker.py
@@ -1,5 +1,3 @@
-print("Welcome to task tracker")
-
 def change_tracker():
 
     with open("task_tracker.txt","r") as f:
@@ -15,7 +13,7 @@ def change_tracker():
             print("Invalid Input")   
 
     c=input("Now enter new task : ")
-             
+
     a.update({f"Task {n}" : c})   
 
 def new_tracker():
@@ -35,17 +33,49 @@ def new_tracker():
                 print("Please enter your task ")
             else:
                 break
-        l.append(m)
-        
 
-    for i,j in zip(range(1,n+1),l):
+    for i,j in zip(range(1,n+1),d):
         print(f"Your task {i} is {j}.")
             
 
 
-    for i,j in zip(range(1,n+1),l):
-        d[f"Task {i}"] = j
-    print(d)  
+    for i,j in zip(range(1,n+1),d):
+        d.update({f"Task {i}":f"{j}"})
+    print()  
 
     with open("task_tracker.txt","w+") as f:
         f.write(str(d))
+
+def new_task():
+    with open("task_tracker.txt","r") as f:
+        print("Your task is given below")
+        a=f.read()
+        print(a)
+
+    x=len(a)+1
+
+    y=input("Now write the task you want to add (you can only add a single task in one time): ")  
+
+    a.update({f"Task {x}" : y})  
+
+
+print("Welcome to task tracker")    
+
+while True:
+        try:
+            g=int(input("Enter what do you want \n If you want to make new task list (Enter 1) \n If  you want to change task list (Enter 2) \n If you want to add new task (Enter 3) : ")) 
+            if (g==1 or g==2 or g==3):
+                break
+            else:
+                print("Please enter 1/2/3 only")
+        except (ValueError,TypeError,SyntaxError,KeyError):
+            print("Invalid Input")  
+
+if g==1 :     
+    new_tracker()
+elif g==2 :
+    change_tracker()
+elif g==3 :
+    new_task()
+else :
+    print("Something Went Wrong")    
