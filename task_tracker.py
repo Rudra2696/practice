0254e6541a452b531ec455f69c2c9e13625f1684
Rudra Patel <rhpatel1023@gmail.com>
2025-03-22 14:32:03 +0530

    Working on new feature

--- task_tracker.py
+++ task_tracker.py
@@ -30,6 +30,31 @@ def change_tracker():
         print(f"Your task is changed\nYour new task is given below\n{tasks}")
     else:
         print("No new task entered. Task not updated.")
+def remove_tracker():
+
+    with open("task_tracker.txt", "r") as f:
+        print("Your task is given below")
+        a = f.read()
+
+    try:
+        tasks = eval(a)
+    except (SyntaxError, NameError):
+        tasks = {}
+        print("Error: Unable to read tasks from file. Starting with an empty task list.")
+
+    print(a)
+
+    while True:
+        try:
+            o = int(input("Enter the number of task you want to remove (integer only): "))
+            if f"Task {o}" not in tasks:
+                print("Task number does not exist. Please enter a valid task number.")
+                continue
+            break
+        except ValueError:
+            print("Invalid Input. Please enter an integer.")   
+    b=dict(tasks)
+    b.pop(o)
 
 def new_tracker():
 
@@ -90,23 +115,28 @@ print("Welcome to task tracker")
 
 while True:
         try:
-            g=int(input("Enter what do you want \n If you want to make new task list (Enter 1) \n If  you want to change task list (Enter 2) \n If you want to add new task (Enter 3) : ")) 
-            if (g==1 or g==2 or g==3):
+            g=int(input("Enter what do you want \n If you want to make new task list (Enter 1) \n "
+            "If  you want to change task list (Enter 2) \n If you want to add new task (Enter 3) \n"
+            "IF youn want tpo remove any task (Enter 4): ")) 
+            if (g==1 or g==2 or g==3 or g==4):
                 break
             else:
                 print("Please enter 1/2/3 only")
         except (ValueError,TypeError,SyntaxError,KeyError):
-            print("Invalid Input")  
+            print("Invalid Input") 
+
 
 if g==1 :     
     new_tracker()
-elif (g==2 or g==3):
+elif (g==2 or g==3 or g==4):
     try:
         with open("task_tracker.txt","r") as f:
             if g==2:
                 change_tracker()
             elif g==3:
                 add_tracker()
+            elif g==4:
+                pass    
     except FileNotFoundError:
         print("task_tracker.txt file not found")
 else :
