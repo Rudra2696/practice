e8ef75b464d14cb5206cd61aeab288846c1c58c0
Rudra Patel <rhpatel1023@gmail.com>
2025-03-24 16:45:13 +0530

    under testing

---
+++ password_genrater.py
@@ -0,0 +1,10 @@
+import random
+a1=random.randint(0,9)
+a2=random.randint(97,122)
+a3=random.randint(65,90)
+a4=random.randint(33,47)
+a5=random.randint(58,64)
+a6=random.randint(91,96)
+a7=random.randint(123,126)
+a8=random.randint(127,255)
+print((a1),chr(a2),chr(a3),chr(a4),chr(a5),chr(a6),chr(a7),chr(a8))
