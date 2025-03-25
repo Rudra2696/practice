544f595baa686ae8b480e57165648b212a8cf7ae
Rudra Patel <rhpatel1023@gmail.com>
2025-03-25 23:41:37 +0530

    add numbers in list

--- even_printer.py
+++ even_printer.py
@@ -9,6 +9,7 @@ for i in range(1,n+1):
     while True:
         try:
             m=int(input(f"Enter any integer {i} : "))
+            l.append(m)
             break
         except (ValueError,TypeError,SyntaxError):  
             print("Invalid Input")
