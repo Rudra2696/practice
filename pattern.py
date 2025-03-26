86490eb6ded67a7ae93c5c12dc0a9082f24ceaf7
Rudra Patel <rhpatel1023@gmail.com>
2025-03-26 20:44:02 +0530

    pattern-3

--- pattern.py
+++ pattern.py
@@ -17,5 +17,15 @@
 # *****
 
 
-for i in range(1,6):                
-    print(" "*(5-i)+"*"*i)  
+# for i in range(1,6):                
+#     print(" "*(5-i)+"*"*i)  
+
+#print below pattern
+# *****
+# ****
+# ***
+# **
+# *
+
+for i in range(5,0,-1):                
+    print("*"*i)
