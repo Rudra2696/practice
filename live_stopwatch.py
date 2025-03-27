5871400b09246094cd97c379761c11ea4d3edecd
Rudra Patel <rhpatel1023@gmail.com>
2025-03-27 20:53:19 +0530

    under testing

---
+++ live_stopwatch.py
@@ -0,0 +1,15 @@
+import time
+
+def countdown_timer(seconds):
+    while seconds:
+        mins, secs = divmod(seconds, 60)  
+        timer = f"{mins:02}:{secs:02}" 
+        print(timer, end="\r")  
+        time.sleep(2)
+        seconds -= 1
+
+    print("Time's up! 🎉")
+
+
+seconds = int(input("Enter the time in seconds: "))
+countdown_timer(seconds)
