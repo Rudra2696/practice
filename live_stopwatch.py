e5613e87be6f6511b4d3c072e553dd0ee1c98a31
Rudra Patel <rhpatel1023@gmail.com>
2025-03-27 20:54:49 +0530

    more efficient

--- live_stopwatch.py
+++ live_stopwatch.py
@@ -5,7 +5,7 @@ def countdown_timer(seconds):
         mins, secs = divmod(seconds, 60)  
         timer = f"{mins:02}:{secs:02}" 
         print(timer, end="\r")  
-        time.sleep(2)
+        time.sleep(1)
         seconds -= 1
 
     print("Time's up! 🎉")
