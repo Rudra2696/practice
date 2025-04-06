0722f34bb53e5467547de5cfff1b41aaa94e9a1e
Rudra Patel <rhpatel1023@gmail.com>
2025-04-06 11:27:26 +0530

    set limit

---
+++ mysql_practice_10.py
@@ -0,0 +1,20 @@
+import pymysql
+
+my=pymysql.connect(host="localhost",user="root",password="",database="Hello")
+
+cur=my.cursor()
+
+print("{:<15}{:<15}{:<15}".format("ID","Name","Marks"))
+
+try:
+    sql="select * from student  limit 0,2"
+
+    cur.execute(sql)
+
+    data=cur.fetchall()
+
+    for i in data:
+        print("{:<15}{:<15}{:<15}".format(i[0],i[1],i[2]))
+
+except Exception:
+    print("Error")
