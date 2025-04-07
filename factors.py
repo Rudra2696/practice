f51807a809ec84be34389a74d70e32c1c120e16b
Rudra Patel <rhpatel1023@gmail.com>
2025-04-07 20:43:43 +0530

    fixed error

--- factors.py
+++ factors.py
@@ -1,7 +1,11 @@
-n=int(input("Enter any integer : "))
+while True:
+    try:
+        n=int(input("Enter any integer : "))
+    except (ValueError,SyntaxError,TypeError)
+    print("please enter integer only")
 l=[]
 for i in range(1,n+1):
     if n%i==0:
         l.append(i)
 
-print(f"THe factors of {n} are {l}")        
+print(f"The factors of {n} are {l}")        
