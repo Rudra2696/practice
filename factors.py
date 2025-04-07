8af9458cc7e8561974ed74dbae708c207aaeb0f0
Rudra Patel <rhpatel1023@gmail.com>
2025-04-07 20:45:53 +0530

    fixed another error

--- factors.py
+++ factors.py
@@ -1,8 +1,9 @@
 while True:
     try:
         n=int(input("Enter any integer : "))
-    except (ValueError,SyntaxError,TypeError)
-    print("please enter integer only")
+        break
+    except (ValueError,SyntaxError,TypeError):
+        print("please enter integer only")
 l=[]
 for i in range(1,n+1):
     if n%i==0:
