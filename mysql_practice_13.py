aeb052f4a5cb9462773d608578ae6371a77d6cab
Rudra Patel <rhpatel1023@gmail.com>
2025-04-08 13:08:15 +0530

    used inner join

---
+++ mysql_practice_13.py
@@ -0,0 +1,20 @@
+import pymysql
+
+my=pymysql.connect(host="localhost",user="root",password="",database="Hello")
+
+cur=my.cursor()
+print("{:<15}{:<15}{:<15}".format("ID","Name","Surename"))
+
+try:
+    sql="select student.id,student.name,result.surnameame from student inner join result on student.id=result.id"
+
+    cur.execute(sql)
+
+    data=cur.fetchall()
+
+    for i in data:
+        print("{:<15}{:<15}{:<15}".format(i[0],i[1],i[2]))
+
+except :
+    print("Error")
+
