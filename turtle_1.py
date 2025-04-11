54b190491ea51988d6bafd567681b63af6465da5
Rudra Patel <rhpatel1023@gmail.com>
2025-04-11 17:48:12 +0530

    added two  different colour

---
+++ turtle_1.py
@@ -0,0 +1,10 @@
+import turtle
+
+sc=turtle.Screen()
+r=turtle.Turtle()
+r.pen(pencolor="red",fillcolor="black",speed=9,pensize=5)
+r.shape("turtle")
+r.begin_fill()
+r.circle(100)
+r.end_fill()
+turtle.done()
