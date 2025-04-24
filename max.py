0a60ac6abb9a97579c6936d1154b39eda7fe8c60
Rudra Patel <rhpatel1023@gmail.com>
2025-04-24 21:43:00 +0530

    give maximum from n numbers

--- max.py
+++ max.py
@@ -1,30 +1,20 @@
-def max(a,b,c):
-    if a>b and a>c:
-        return a
-    elif b>a and b>c:
-        return b
-    else:
-        return c    
    
-while True:
-    try:    
-        a=int(input("Enter 1st number : "))
-        break
-    except (ValueError,TypeError,SyntaxError):
-        print("Invalid Input")
-   
-while True:
-    try:    
-        b=int(input("Enter 2nd number : "))
-        break
-    except (ValueError,TypeError,SyntaxError):
-        print("Invalid Input")   
+l=[]
 
 while True:
     try:    
-        c=int(input("Enter 3rd number : "))
+        a=int(input("Enter number of elements : "))
         break
     except (ValueError,TypeError,SyntaxError):
         print("Invalid Input")
 
-print(max(a,b,c))       
+for i in range(1,a+1):   
+    while True:
+        try:    
+            i=int(input(f"Enter {i} number : "))
+            l.append(i)
+            break
+        except (ValueError,TypeError,SyntaxError):
+            print("Invalid Input")   
+
+print(max(l))
