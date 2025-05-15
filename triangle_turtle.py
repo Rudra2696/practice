054ec0a5914214703085284d5a33251c12a70433
Rudra Patel <rhpatel1023@gmail.com>
2025-05-15 14:58:04 +0530

    used for loop

--- triangle_turtle.py
+++ triangle_turtle.py
@@ -3,10 +3,8 @@ import turtle
 r=turtle.Turtle()
 r.color("red")
 r.begin_fill()
-r.forward(100)
-r.left(120)
-r.forward(100)
-r.left(120)
-r.forward(100)
+for i in range(3):
+    r.forward(100)
+    r.left(120)
 r.end_fill()
 turtle.done()
